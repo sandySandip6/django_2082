4e4b30b3fec8c5e05f1f50da600f1621d1d4503b
Sandy <codingsandy6@gmail.com>
2025-04-16 16:55:30 +0545

    Used the render function to load the template

--- mypost/polls/views.py
+++ mypost/polls/views.py
@@ -4,28 +4,26 @@
 
 # def index(request):
 #     return render(request, 'this is the index page')
-from django.http import Http404
-from django.http import HttpResponse
-from django.template import loader
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.http import Http404 
 from .models import Question
 
 
 def polls_index(request):
     latest_question_list = Question.objects.order_by("-post_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html",context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+def detail(request, qns_id):
+    try:
+        question = Question.objects.get(pk = qns_id)
+    except Question.DoesNotExist:
+        raise Http404("Qestion Not Found")
     return render(request, "polls/detail.html", {"question": question})
 
 def result(request, qns_id):
     response = "you are looking at the result of question %s."
-    return HttpResponse(response%qns_id)
+    return render(request, response%qns_id)
 
 def vote(request, qns_id):
-    return HttpResponse("you are voting on %s.", qns_id)
-
-
+    return render(request,"you are voting on %s.", qns_id)
