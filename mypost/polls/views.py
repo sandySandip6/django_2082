cbdb4bfad66f63f6ec3f35c505ff73a07b1b6499
Sandy <codingsandy6@gmail.com>
2025-04-18 23:48:06 +0545

    added the polls file in template of polls

--- mypost/polls/views.py
+++ mypost/polls/views.py
@@ -1,9 +1,4 @@
-# from django.shortcuts import render
 
-# # Create your views here.
-
-# def index(request):
-#     return render(request, 'this is the index page')
 from django.shortcuts import render
 from django.http import Http404 
 from .models import Question
